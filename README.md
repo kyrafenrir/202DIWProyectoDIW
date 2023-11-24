9cfc5b379fba4cc1d09121b3ff1fe9a6de3e82bc
Erika Martínez <erikamp1112@gmail.com>
2023-11-24 01:44:42 +0100

    Version 1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# Proyecto DIW
+## Autor
+Erika Martínez Pérez
+## Requisitos minimos del desarrollo
+- [Apache2.4.52]
+- [MySQL8.0.35]
+- [PHP8.1]
+- [XDebug3.1.2]
+## Descripcion
+Documentacion de la asignatura diseño de interfaces web / DAW
+## Funcionalidades
+## Entorno de desarrollo
+[http://daw202.isauces.local/202DIWProyectoDIW/indexProyectoDIW.php]
+(Entorno de desarrollo de Erika)
+## Entorno de explotacion
+[http://daw202.ieslossauces.es/202DIWProyectoDIW/indexProyectoDIW.php]
+(Entorno de explotación de Erika)
---
+++ indexProyectoDIW.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="/webroot/css/style.css">
+        <title>DWES</title>
+    </head>
+    <body>
+        <header>
+            <h1>Diseño de interfaces web</h1>
+        </header>
+        <p>Profesor: Antonio Jañez Veleda</p>
+        <footer>
+            <p>2023-2024 © Todos los derechos reservados. <a href="../index.html">Erika Martínez Pérez</a></p>
+        </footer>
+    </body>
+</html>
